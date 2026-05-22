21e5c3b5e106839242361b2208070b2a5b3c63e9
Amaan Yakub <amaanyakub99@gmail.com>
2026-05-21 19:58:43 -0500

    Point frontend API to Render backend in production

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api', withCredentials: true });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api',
+  withCredentials: true,
+});
 export default api;
